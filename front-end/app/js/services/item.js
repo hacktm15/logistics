eb60cd2050309fc7f06c0c66110f80cc8022f4bb
AlexandruDoru <alexandru.doru92@gmail.com>
2015-11-08 09:37:58 +0200

    adds changes

--- front-end/app/js/services/item.js
+++ front-end/app/js/services/item.js
@@ -2,7 +2,13 @@ logisticsApp.factory('itemService', ['$http', function ($http) {
   return {
     all: function (params) {
       queryString = '';
-      queryString += 'substringof(tolower(\''+params.Name+'\'), tolower(Name)) eq true'
+
+      if (params.Name != undefined) {
+        queryString += 'substringof(tolower(\''+params.Name+'\'), tolower(Name)) eq true'
+      }
+      if (params.Location != undefined) {
+
+      }
       console.log(queryString);
 
       return $http.get("http://tools.ligaac.ro/oData/Item?$filter="+queryString);
